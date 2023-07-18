64d0e3e04132dcf6f7668b8b6615646c89fc638b
Carl Downs <carl.downs@oracle.com>
2023-07-18 16:20:31 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,201 @@
+# Exporting OCI Monitoring Service Metrics to MoogSoft
+
+---
+
+## Overview
+
+Let's take a look at bringing Oracle Cloud Infrastructure (OCI)’s rich Metrics resources over to 
+MoogSoft to accomplish common goals such DevOps monitoring, application performance monitoring, and so on. 
+MoogSoft’s API exposes some unique features. Their API allows you to characterize each metric using tags, 
+which is essential to aggregating and correlating data as needed for its monitoring, reporting, dashboards, and so on.
+
+See [MoogSoft's Integration API Overview](https://api.docs.moogsoft.com/docs/latest/branches/main/165686df52254-metrics-integration-api-overview)
+and the [MoogSoft format specification](https://api.docs.moogsoft.com/docs/latest/branches/main/7e62a06cd4faa-metric-datum-api-object) for details.
+
+## Prerequisites
+
+If you’re new to Functions, get familiar by running through 
+the [Quick Start guide on OCI Functions](http://docs.oracle.com/en-us/iaas/Content/Functions/Tasks/functionsquickstartguidestop.htm) before proceeding.
+
+---
+## Solution Architecture
+
+![](images/architecture.png)
+
+Here is the basic architecture and flow of data from beginning to end:
+
+* OCI services emit metric data which is captured by the Monitoring service.
+* The Monitoring Service feeds metric data events to a Service Connector.
+* The Service Connector invokes a Function which transforms the metric data payload to MoogSoft format and posts the transformed payload to the MoogSoft REST API.
+* MoogSoft ingests the metrics, building its own aggregations using the provided tagging.
+
+Let's drill down into the OCI Services involved.
+
+---
+## Monitoring Service
+
+ The [Monitoring Service](https://docs.oracle.com/en-us/iaas/Content/Monitoring/Concepts/monitoringoverview.htm)
+ receives timestamp-value pairs (aka metric data points) which also carry contextual 
+dimensions and metadata about the services or applications that emitted them. 
+
+---
+## Service Connector Hub
+
+The stream of Metric data is event-driven and must be handled on-demand and at scale. The 
+[Service Connector Hub](https://docs.oracle.com/en-us/iaas/Content/service-connector-hub/overview.htm) does
+exactly that.  See [Service Connector Hub documentation](https://docs.oracle.com/en-us/iaas/Content/service-connector-hub/overview.htm) for details.
+
+---
+## Functions Service
+
+I need to transform between the raw metrics formats and some way to make the MoogSoft API calls. The 
+[OCI Functions Service](http://docs.oracle.com/en-us/iaas/Content/Functions/Concepts/functionsoverview.htm) is a 
+natural fit for the task. Functions integrate nicely with Service Connector Hub as as a target and can scale up
+depending on the demand.  That lets me focus on writing the logic needed without needing to address how to 
+deploy and scale it.
+
+---
+## Mapping From OCI to MoogSoft Formats
+
+A key requirement of course is the mapping of OCI to MoogSoft format.  Let's compare the OCI and MoogSoft
+message payload formats, what the mapping needs to accomplish, and see what the resulting transformed message 
+looks like.
+
+Example OCI Metrics Payload:
+    
+    {
+      "namespace": "oci_vcn",
+      "resourceGroup": null,
+      "compartmentId": "ocid1.compartment.oc1...",
+      "name": "VnicFromNetworkMirrorBytes",
+      "dimensions": {
+        "resourceId": "ocid1.vnic.oc1.phx..."
+      },
+      "metadata": {
+        "displayName": "Mirrored Bytes from Network",
+        "unit": "bytes"
+      },
+      "datapoints": [
+        {
+          "timestamp": 1652196492000,
+          "value": 6.0,
+          "count": 1
+        }
+      ]
+    }
+
+Example MoogSoft Metrics Payload 
+(see [MoogSoft format specification](https://api.docs.moogsoft.com/docs/latest/branches/main/7e62a06cd4faa-metric-datum-api-object)):
+
+    {
+        "metric": ""myApp response time msec"m
+        "source": "source.server.com", 
+        "time": 1571435450540, 
+        "data": 1.2345, 
+        "tags": {
+            "protocol" : "tcp",
+            "seq" : 12, 
+            "total" : 157, 
+            "bytes" : 1274
+        } 
+    }
+
+Mapping Behavior:
+
+    {
+      "metric": "{metdata.displayName}",
+      "source": "{re-characterized OCI namespace and metric name values}",
+      "time": {datapoint.timestamp},
+      "data": {datapoint.value}
+      ],
+      "tags": [
+        "{metrics tag key1:oci payload value}",
+        "{metrics tag key2:oci payload_value}"
+      ]
+    }
+
+Resulting Output:
+
+    {
+      "metric": "Mirrored Bytes from Network",
+      "source": "oci.vcn.vnic.from.network.mirror.bytes",
+      "time": 1652196492000,
+      "data": 6.0,
+      "tags": [
+        "displayName:Mirrored Bytes from Network",
+        "name:VnicFromNetworkMirrorBytes",
+        "unit:bytes",
+        "namespace:oci_vcn"
+      ]
+    }
+
+---
+## Policy Setup
+
+You will need 
+this [IAM policy](https://docs.oracle.com/en-us/iaas/Content/Functions/Tasks/functionscreatingpolicies.htm#Create_Policies_to_Control_Access_to_Network_and_FunctionRelated_Resources) 
+to authorize the Service Connector to invoke your Function.
+
+    allow any-user to use fn-function in compartment id ocid1.compartment.oc1... where all {request.principal.type=’serviceconnector’, request.principal.compartment.id=’ocid1.compartment.oc1...’}
+
+---
+## Service Connector Setup
+
+Now let’s set up a simple service connector instance that takes Monitoring sources and passes them to our Function.
+
+Because your Function requires a VCN, you can use that VCN as the metric source to test against.  Let's test
+with the `oci_vcn` Monitoring namespace because it will quickly generate a lot of useful events.
+
+Select Monitoring as the source and the Function as the target. Configure your source as the 
+compartment where the VCN resides and select the Monitoring namespace (`oci_vcn`) that you want to
+pick up. Select your Application and the Function within it as the target.
+
+<br />
+
+[<img src="images/sch-setup.png" width="800"/>](image.png)
+
+---
+## View Metrics In MoogSoft
+
+When you have the Service Connector configured, metrics appear in MoogSoft's Metrics Explorer and notebooks 
+after a few minutes.
+
+
+## Function Environment
+
+Here are the supported Function parameters:
+
+| Environment Variable        | Default           | Purpose                                                                            |
+| ------------- |:-------------:|:-----------------------------------------------------------------------------------|
+| API_ENDPOINT      | not-configured | REST API endpoint for reaching MoogSoft                                            |
+| API_TOKEN      | not-configured      | API license token obtained from MoogSoft                                           |
+| METRICS_TAG_KEYS | name, namespace, displayName, resourceDisplayName, unit      | OCI Metric Dimensions and metadata to convert to MoogSoft Metric Tags              |
+| LOGGING_LEVEL | INFO     | Controls function logging outputs.  Choices: INFO, WARN, CRITICAL, ERROR, DEBUG    |
+| FORWARDING_ENABLED | True      | Determines whether messages are forwarded to MoogSoft                              |
+
+---
+## Conclusion
+
+You now have a low-maintenance, serverless function that can send raw metrics over to MoogSoft in
+near-real time. I encourage you to experiment with the dimensions and metadata tag mappings 
+to see which combination works best for your use case.  
+
+See [MoogSoft UI Reference](https://docs.moogsoft.com/moogsoft-cloud/en/moogsoft-ui-reference.html).
+
+---
+## **OCI** Related Workshops
+
+LiveLabs is the place to explore Oracle's products and services using workshops designed to 
+enhance your experience building and deploying applications on the Cloud and On-Premises.
+ur library of workshops cover everything from how to provision the world's first autonomous 
+database to setting up a webserver on our world class OCI Generation 2 infrastructure, 
+machine learning and much more.  Use your existing Oracle Cloud account, 
+a [Free Tier](https://www.oracle.com/cloud/free/) account or a LiveLabs Cloud Account to build, test, 
+and deploy applications on Oracle's Cloud.
+
+Visit [LiveLabs](http://bit.ly/golivelabs) now to get started.  Workshops are added weekly, please visit frequently for new content.
+
+---
+## License
+Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
---
+++ func.py
@@ -0,0 +1,279 @@
+#
+# oci-moogsoft-observability version 1.0.
+#
+# Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+import io
+import json
+import logging
+import os
+import re
+import requests
+from datetime import datetime
+
+"""
+    This sample OCI Function maps OCI Monitoring Service Raw Metrics to the MoogSoft 
+    See https://api.docs.moogsoft.com/docs/latest/branches/main/165686df52254-metrics-integration-api-overview
+    See https://api.docs.moogsoft.com/docs/latest/branches/main/7e62a06cd4faa-metric-datum-api-object
+"""
+
+# Use OCI Application or Function configurations to override these environment variable defaults.
+
+api_endpoint = os.getenv('API_ENDPOINT', 'not-configured')
+api_key = os.getenv('API_KEY', 'not-configured')
+is_forwarding = eval(os.getenv('FORWARDING_ENABLED', "False"))
+
+tag_keys = os.getenv('TAG_KEYS', 'name, namespace, displayName, resourceDisplayName, unit')
+tag_set = set()
+
+# Set all registered loggers to the configured log_level
+
+logging_level = os.getenv('LOGGING_LEVEL', 'INFO')
+loggers = [logging.getLogger()] + [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+[logger.setLevel(logging.getLevelName(logging_level)) for logger in loggers]
+
+# Constants
+
+TEN_MINUTES_SEC = 10 * 60
+ONE_HOUR_SEC = 60 * 60
+
+"""
+"""
+
+
+def handler(ctx, data: io.BytesIO = None):
+    """
+    OCI Function Entry Point
+    :param ctx: InvokeContext
+    :param data: data payload
+    :return: plain text response indicating success or error
+    """
+
+    preamble = " {} / event count = {} / logging level = {} / forwarding to MoogSoft = {}"
+
+    try:
+        metrics_list = json.loads(data.getvalue())
+        logging.info(preamble.format(ctx.FnName(), len(metrics_list), logging_level, is_forwarding))
+
+        # logging.debug(metrics_list)
+
+        transformed_event_list = transform_metric_events(event_list=metrics_list)
+        send_to_moogsoft_endpoint(event_list=transformed_event_list)
+
+    except (Exception, ValueError) as ex:
+        logging.error('error handling logging payload: {}'.format(str(ex)))
+        logging.error(ex)
+
+
+def transform_metric_events(event_list):
+    """
+    :param event_list: the list of metric formatted log records.
+    :return: the list of MoogSoft formatted log records
+    """
+
+    result_list = []
+    for event in event_list:
+        single_result = transform_metric_to_moogsoft_format(log_record=event)
+        result_list.append(single_result)
+        logging.debug(single_result)
+
+    return result_list
+
+
+def transform_metric_to_moogsoft_format(log_record: dict):
+    """
+    Transform metrics to MoogSoft format.
+    MoogSoft is expecting Unix time ... see https://en.wikipedia.org/wiki/Unix_time
+    :param log_record: metric log record
+    :return: MoogSoft formatted log record
+    """
+
+    payload = []
+    data_points = get_data_points(log_record)
+    for dp in data_points:
+        transformed_record = {
+                'metric': get_dictionary_value(log_record, 'displayName'),
+                'source': get_source(log_record),
+                'time': dp.get('timestamp'),
+                'data': dp.get('value'),
+                'tags': get_tags(log_record),
+            }
+        payload.append(transformed_record)
+
+    return payload
+
+
+def get_source(log_record: dict):
+    """
+    Assembles a metric name that is compatible with MoogSoft.
+    :param log_record:
+    :return:
+    """
+
+    elements = get_dictionary_value(log_record, 'namespace').split('_')
+    elements += camel_case_split(get_dictionary_value(log_record, 'name'))
+    elements = [element.lower() for element in elements]
+    return '.'.join(elements)
+
+
+def camel_case_split(string):
+    """
+    :param string:
+    :return: Splits camel case string to individual strings
+    """
+
+    return re.findall(r'[A-Z](?:[a-z]+|[A-Z]*(?=[A-Z]|$))', string)
+
+
+def get_now_timestamp():
+    return datetime.now().timestamp()
+
+
+def get_data_points(log_record: dict):
+    """
+    :param log_record:
+    :return: an array of arrays where each array is a datapoint scalar pair
+    """
+
+    result = []
+
+    datapoints = get_dictionary_value(dictionary=log_record, target_key='datapoints')
+    for point in datapoints:
+        data_point = {'timestamp': point.get('timestamp'), 'value': point.get('value')}
+
+        result.append(data_point)
+
+    return result
+
+
+def get_tags(log_record: dict):
+    """
+    Assembles tags from selected metric attributes.
+    See https://docs.MoogSofthq.com/getting_started/tagging/
+    :param log_record: the log record to scan
+    :return: string of comma-separated, key:value pairs matching MoogSoft tag format
+    """
+
+    result = []
+
+    for tag in get_tag_set():
+        value = get_dictionary_value(dictionary=log_record, target_key=tag)
+        if value is None:
+            continue
+
+        if isinstance(value, str) and ':' in value:
+            logging.warning('tag contains a \':\' / ignoring {} ({})'.format(tag, value))
+            continue
+
+        tag = '{}:{}'.format(tag, value)
+        result.append(tag)
+
+    return result
+
+
+def get_tag_set():
+    """
+    :return: the set metric payload keys that are to be converted to a tag.
+    """
+
+    global tag_set
+
+    if len(tag_set) == 0 and tag_keys:
+        split_and_stripped_tags = [x.strip() for x in tag_keys.split(',')]
+        tag_set.update(split_and_stripped_tags)
+        logging.debug("tag key set / {} ".format(tag_set))
+
+    return tag_set
+
+
+def send_to_moogsoft_endpoint(event_list):
+    """
+    Sends each transformed event to API Endpoint.
+    :param event_list: list of events in Moogsoft format
+    :return: None
+    """
+
+    if is_forwarding is False:
+        logging.info("MoogSoft forwarding is disabled - nothing sent")
+        logging.info(json.dumps(event_list, indent=2))
+        return
+
+    # creating a session and adapter to avoid recreating
+    # a new connection pool between each POST call
+
+    try:
+        session = requests.Session()
+        adapter = requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=10)
+        session.mount('https://', adapter)
+
+        for event in event_list:
+            api_headers = {'Content-type': 'application/json', 'API-KEY': api_key}
+            logging.debug("json to MoogSoft: {}".format(json.dumps(event)))
+            response = session.post(api_endpoint, data=json.dumps(event), headers=api_headers)
+
+            if response.status_code not in (200, 201, 202):
+                raise Exception('error {} sending to MoogSoft: {}'.format(response.status_code, response.reason))
+
+    finally:
+        session.close()
+
+
+def get_dictionary_value(dictionary: dict, target_key: str):
+    """
+    Recursive method to find value within a dictionary which may also have nested lists / dictionaries.
+    :param dictionary: the dictionary to scan
+    :param target_key: the key we are looking for
+    :return: If a target_key exists multiple times in the dictionary, the first one found will be returned.
+    """
+
+    if dictionary is None:
+        raise Exception('dictionary None for key'.format(target_key))
+
+    target_value = dictionary.get(target_key)
+    if target_value:
+        return target_value
+
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            target_value = get_dictionary_value(dictionary=value, target_key=target_key)
+            if target_value:
+                return target_value
+
+        elif isinstance(value, list):
+            for entry in value:
+                if isinstance(entry, dict):
+                    target_value = get_dictionary_value(dictionary=entry, target_key=target_key)
+                    if target_value:
+                        return target_value
+
+
+def local_test_mode(filename):
+    """
+    This routine reads a local json metrics file, converting the contents to MoogSoft format.
+    :param filename: cloud events json file exported from OCI Logging UI or CLI.
+    :return: None
+    """
+
+    logging.info("local testing started")
+
+    with open(filename, 'r') as f:
+        transformed_results = list()
+
+        for line in f:
+            event = json.loads(line)
+            logging.debug(json.dumps(event, indent=4))
+            transformed_results += transform_metric_to_moogsoft_format(event)
+
+        logging.debug(json.dumps(transformed_results, indent=4))
+        send_to_moogsoft_endpoint(event_list=transformed_results)
+
+    logging.info("local testing completed")
+
+
+"""
+Local Debugging 
+"""
+
+if __name__ == "__main__":
+    local_test_mode('oci-metrics-test-file.json')
